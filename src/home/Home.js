2525f5a584ead1b9aae04c14099ae1ae6b98c1c2
Agatadell12 <149382123+Agatadell12@users.noreply.github.com>
2024-06-04 21:10:43 +0200

    wip: finish page home, start services

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,15 +1,26 @@
 import React from "react";
 import "./Home.css";
-import home from "./home.jpg";
+import mundury_1 from "./mundury_1.jpg";
 
 function Home({ text }) {
   return (
     <div className="home">
-      {/*Home_left */}
-      <div className="home_left"></div>
-      <img src={home} />
+      <div className="home_top">
+        <button className="home_top_about">O PROJEKCIE</button>
+      </div>
+      <div className="home_button"></div>
+      <div className="home_left">
+        <div className="home_left_title">GEOPORTAL</div>
+        <div className="home_left_subtitle">
+          Geoportal tematyczny sytemu zarządzania „mundurówka”
+        </div>
+
+        <button className="home_right_button"> START </button>
+      </div>
       {/*Home_right*/}
-      <div className="home_right"></div>
+      <div className="home_right">
+        <img className="mundury_1" src={mundury_1} alt="logo" />
+      </div>
     </div>
   );
 }
